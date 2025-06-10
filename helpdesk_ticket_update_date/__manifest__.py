a4e36c6152853bb4b3b7a3363b645d1a86cff1f4
Ane <aneavanzosc@gmail.com>
2025-06-10 12:39:10 +0200

    [16.0][ADD] helpdesk_ticket_update_date: Add new fields to view

---
+++ helpdesk_ticket_update_date/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Helpdesk Ticket Update Date",
+    "version": "16.0.1.0.0",
+    "category": "Helpdesk",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "depends": ["helpdesk"],
+    "data": [
+        "views/helpdesk_tickets_followup.xml",
+        "views/helpdesk_portal_helpdesk_ticket.xml"
+    ],
+    "installable": True,
+    "application": False
+}
