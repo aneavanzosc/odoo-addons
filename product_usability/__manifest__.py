cbc2151d9261bc437e24f7efbbe95e4716978948
Ane <aneavanzosc@gmail.com>
2025-09-08 10:03:37 +0200

    [18.0][MIG] product_usability: Migration to 18.0

--- product_usability/__manifest__.py
+++ product_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Product Usability",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales/Sales",
     "license": "AGPL-3",
     "author": "AvanzOSC",
