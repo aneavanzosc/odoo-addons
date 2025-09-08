7acd7a7b49c41193c0a22f97e4c9935cd30ba47d
Ane <aneavanzosc@gmail.com>
2025-09-08 15:31:06 +0200

    [18.0][MIG] product_supplierinfo_for_customer_ext: Migration to 18.0

--- product_supplierinfo_for_customer_ext/__manifest__.py
+++ product_supplierinfo_for_customer_ext/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Product Supplierinfo for Customer Ext",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
-    "depends": ["product_supplierinfo_for_customer"],
+    "depends": ["product_customerinfo"],
     "data": [
         "data/product_supplierinfo_for_customer_ext_data.xml",
     ],
