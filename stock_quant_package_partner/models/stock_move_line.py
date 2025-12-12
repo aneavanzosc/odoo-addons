3fa07c36550b9c3ce986a8f896285c4a47289d5c
Ane <aneavanzosc@gmail.com>
2025-12-12 12:04:04 +0100

    [16.0][FIX] stock_quant_package_partner: Remove related field for partner in stock.move.line and copy partner from picking when creating the move line

--- stock_quant_package_partner/models/stock_move_line.py
+++ stock_quant_package_partner/models/stock_move_line.py
@@ -1,8 +1,10 @@
-from odoo import models
+from odoo import api, fields, models
 
 
 class StockMoveLine(models.Model):
     _inherit = "stock.move.line"
+
+    partner_id = fields.Many2one("res.partner", string="Partner")
 
     def _action_done(self):
         res = super()._action_done()
@@ -11,3 +13,18 @@ class StockMoveLine(models.Model):
                 ml.package_id.partner_id = ml.move_id.picking_id.partner_id
 
         return res
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        mls = super().create(vals_list)
+        for ml in mls:
+            if not ml.partner_id:
+                if ml.move_id.picking_id and ml.move_id.picking_id.partner_id:
+                    ml.partner_id = ml.move_id.picking_id.partner_id
+        return mls
+
+    @api.onchange("partner_id")
+    def _onchange_partner_id(self):
+        for ml in self:
+            if ml.result_package_id:
+                ml.result_package_id.partner_id = ml.partner_id
