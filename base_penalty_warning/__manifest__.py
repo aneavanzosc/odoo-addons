ef71432e44b5408d5e0cd67810dadff4935c3c78
Ane <aneavanzosc@gmail.com>
2025-12-19 12:14:47 +0100

    [18.0][MIG] base_penalty_warning: Migration to 18.0

--- base_penalty_warning/__manifest__.py
+++ base_penalty_warning/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Base Penalty Warning",
-    "version": "14.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Hidden",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/odoo-addons",
