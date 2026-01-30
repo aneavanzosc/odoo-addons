ad0037e9e808744b4409feb3b9c9c9b6a2e179fd
Ane <aneavanzosc@gmail.com>
2026-01-30 08:36:21 +0100

    [12.0][FIX] product_fsc_certificate: Remove certificate FSC

---
+++ product_fsc_certificate/models/res_company.py
@@ -0,0 +1,8 @@
+# Copyright 2026 Ane Gurruchaga - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+    
+    fsc_certificate_image = fields.Binary(string="FSC Certificate Logo")
