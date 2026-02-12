03bcd0140881555812078c006380d0c812bd28d8
Ane <aneavanzosc@gmail.com>
2026-02-12 15:01:28 +0100

    [14.0][ADD] account_invoice_report_hide_without_reference: The block "Without reference" and its subtotal are removed from the report

---
+++ account_invoice_report_hide_without_reference/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Ane Gurruchaga - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Invoice Report Hide Without Reference",
+    "version": "14.0.1.0.0",
+    "author": "AvanzOSC",
+    "category": "Invoices & Payments",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": ["account_invoice_report_grouped_by_picking"],
+    "data": [
+        "reports/account_invoice_report.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
