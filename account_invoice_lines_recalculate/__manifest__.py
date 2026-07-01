00061f33fa0d89658749b88c22411c990fcf9653
Ane <aneavanzosc@gmail.com>
2026-07-01 14:14:29 +0200

    [18.0][MIG] account_invoice_lines_recalculate: Migration to 18.0

---
+++ account_invoice_lines_recalculate/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Invoice Lines Recalculate",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/odoo-addons",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_move_views.xml",
+        "views/product_category_views.xml",
+        "views/product_product_views.xml",
+    ],
+    "installable": True,
+}
